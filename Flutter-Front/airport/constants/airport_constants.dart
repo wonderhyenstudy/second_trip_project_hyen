15a47d057dfb0e5d0fcbc5b86587ba571bc27416
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-04-19 21:09:38 +0900

    첫커밋

--- Flutter-Front/airport/constants/airport_constants.dart
+++ Flutter-Front/airport/constants/airport_constants.dart
@@ -4,4 +4,5 @@ class AirportConstants {
   // ── 항공 관련 상수 ───────────────────────────────────────
   static const int issueFee = 1000;  // 발급 수수료
   static const int pageSize = 10;    // 페이지 데이터 수
+
 }
